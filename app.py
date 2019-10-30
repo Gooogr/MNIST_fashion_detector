53d28193d9571fd315e6e50fad6554c74322a8c9
Grigoriy <gusarovga@ya.ru>
2019-10-30 17:28:28 +0300

    clean main script

--- app.py
+++ app.py
@@ -2,16 +2,12 @@ from flask import Flask, render_template, request
 import cv2
 from imageio import imread
 import numpy as np
-
 import re
 import sys
 import os
-
 import base64
-
 from keras.models import load_model
 from keras.models import model_from_json
-
 import json
 
 model_weights_path = 'mnist_fashion_model.h5'
@@ -32,6 +28,9 @@ fashion_dict = {0: 'T_shirt',
 app = Flask(__name__)
 
 def init_model():
+    '''
+    Function for initializing keras model from json and h5 files.
+    '''
     # https://stackoverflow.com/questions/53212672/read-only-mode-in-keras
     json_file = open(model_json_path,'r')
     model_json = json_file.read()
@@ -45,12 +44,17 @@ def init_model():
 
 # decoding an image from base64 into raw representation
 def convertImage(imgData1):
+    '''
+    Function decode image from base64 format into raw png representation.
+    input - base64 image from site widget
+    output - saved output.png
+    '''
     imgstr = re.search(r'base64,(.*)', str(imgData1)).group(1)
     with open('output.png', 'wb') as output:
         output.write(base64.b64decode(imgstr))
 
 
-@app.route('/', methods=['GET'])  #add metods here
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
@@ -59,21 +63,19 @@ def index():
 def predict():
     # get the raw data format of the image
     imgData = request.get_data()
-    # encode it into a suitable format
+    # encode it into a suitable format and save result
     convertImage(imgData)
     # read the image into memory
     x = imread('output.png', pilmode = 'L')
-    # make it the right size
+    # make it the right size and shape
     x = cv2.resize(x, (28, 28))
-    # imsave('final_image.jpg', x)
-    # convert to a 4D tensor to feed into our model
     x = x.reshape(1, 28, 28, 1)
 
     model = init_model()
     output = model.predict(x)
     print(output)
-    print(np.argmax(output, axis=1))
     response = np.argmax(output, axis=1)
+    print(fashion_dict[response[0]])
     return str(fashion_dict[response[0]])
 
 if __name__ == "__main__":
